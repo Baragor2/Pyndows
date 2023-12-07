9424f02c9456774af1cfc66206331681a1e43103
Baragor2 <evgen3email@gmail.com>
2023-12-07 19:36:00 +0300

    user's choice

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+class Pyndows:
+    def __init__(self):
+        print(
+'''Hello, Pyndows user!
+What do you want to do?
+1. File system
+2. CLock
+3. Calculator
+4. Memory''')
+        self.choose_func()
+
+    def choose_func(self):
+        num = input()
+
+        match (num):
+            case '1':
+                MyFiles = FileSystem()
+            case _:
+                print("Incorrect input ")
+
+
 class FileSystem:
     files = []
 
     def __init__(self):
         print("FileSystem")
+        print('Enter "help" for more information')
+        if input() == 'help':
+            self.help_files()
+        else:
+            print("Incorrect input ")
+
+    @staticmethod
+    def help_files():
+        print(
+'''1.To create a file, enter "create <filename>"
+2.To remove a file, enter "remove <filename>"
+3.To rename a file, enter "rename <filename>"
+4.To show a list of files, enter "show files"''')
+
+    @classmethod
+    def show_files(cls):
         if not self.files:
             print('Directory is empty')
         else:
-            print("Your files", *self.files)
-        print('Enter "help" for more information')
+            print("Your files:", *self.files)
 
     @classmethod
     def create_file(cls, filename):
@@ -32,23 +68,9 @@ class FileSystem:
                 cls.files[ind] = new_filename
                 return
         print("This file doesn't exist")
+
+os = Pyndows()
 FileSystem.create_file('file')
-
-print(
-'''
-Hello, Pyndows user!
-What do you want to do?
-1. File system
-2. CLock
-3. Calculator
-4. Memory
-'''
-)
-num = input()
-
-match(num):
-    case '1':
-        MyFiles = FileSystem()
 
 
 
